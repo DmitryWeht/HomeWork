83774ec99af93477b5632ca985c2cf8e539d1eb7
DmitryWeht <129493456+DmitryWeht@users.noreply.github.com>
2023-06-06 21:30:59 +0200

    Add files via upload

---
+++ HomeWorkAlgoritm.java
@@ -0,0 +1,34 @@
+package HomeWork;
+
+import java.util.Scanner;
+
+public class HomeWorkAlgoritm {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Введите число от 1 до 7: ");
+        int number = scanner.nextInt();
+
+
+        if (number == 1) {
+            System.out.println("Monday");
+        } else if (number == 2) {
+            System.out.println("Tuesday");
+        } else if (number == 3) {
+            System.out.println("Wednesday");
+        } else if (number == 4) {
+            System.out.println("Thursday");
+        } else if (number == 5) {
+            System.out.println("Friday");
+        } else if (number == 6) {
+            System.out.println("Saturday");
+        } else if (number == 7) {
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Ошибка! Введите число от 1 до 7.");
+        }
+    }
+}
