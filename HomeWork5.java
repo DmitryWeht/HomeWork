a938b6e698e8ea6819aa22b755c09cfd1097732c
DmitryWeht <129493456+DmitryWeht@users.noreply.github.com>
2023-06-11 20:13:04 +0200

    Add files via upload

---
+++ HomeWork5.java
@@ -0,0 +1,12 @@
+package HomeWork;
+
+public class HomeWork5 {
+    public static void main(String[] args) {
+        int num = 1;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(num + " ");
+            num *= 2;
+        }
+    }
+}
